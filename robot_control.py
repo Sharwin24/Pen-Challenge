34a234b674e3fa60548a30bd6cc6c4127b89426d
Sharwin24 <sharwin24@gmail.com>
2024-09-20 10:03:12 -0500

    Moving commands finished

--- robot_control.py
+++ robot_control.py
@@ -28,9 +28,8 @@ class DIRECTION(Enum):
     UP = 0,
     DOWN = 1,
     FORWARD = 2,
-    BACKWARD = 3
-    LEFT = 4,
-    RIGHT = 5
+    BACKWARD = 3,
+    ROTATE = 4
 
 
 class Controller:
@@ -40,7 +39,7 @@ class Controller:
         robot_startup()
         self.joint_dict = {}
         self.update_joint_angle_dict()
-        self.step_size = 10  # cm
+        self.step_size = 5 / 100  # default is 5 cm, convert to meters
 
     def update_joint_angle_dict(self):
         angles = self.robot.arm.get_joint_commands()
@@ -50,16 +49,18 @@ class Controller:
         self.joint_dict["wrist_angle"] = angles[3]
 
     def ee_pos(self):
-        return self.robot.arm.get_ee_pose_command()[3]
+        T = self.robot.arm.get_ee_pose()
+        return (T[0][3], T[1][3], T[2][3])
 
     def parse_inputs(self):
+        ee_pos = self.ee_pos()
         cmd_str = f"\nEnter a command for the robot using one of the commands, some will prompt for more arguments" + \
             f"\nWaist: {np.rad2deg(self.joint_dict["waist"]):.2f}\N{DEGREE SIGN}" + \
             f"\nShoulder: {np.rad2deg(self.joint_dict["shoulder"]):.2f}\N{DEGREE SIGN}" + \
             f"\nElbow: {np.rad2deg(self.joint_dict["elbow"]): .2f}\N{DEGREE SIGN}" + \
             f"\nWrist Angle: {np.rad2deg(self.joint_dict["wrist_angle"]):.2f}\N{DEGREE SIGN}" + \
-            f"\nEE Position: ({self.ee_pos()[0]:.2f}, {self.ee_pos()[1]:.2f}, {self.ee_pos()[3]:.2f})" + \
-            f"\n[h]ome, [s]leep, [q]uit, [u]p, [d]own, [f]orward, [b]ackward, [st]ep_size: "
+            f"\nEE Position: ({100 * ee_pos[0]:.2f}, {100 * ee_pos[1]:.2f}, {100 * ee_pos[2]:.2f}) cm" + \
+            f"\n[h]ome, [s]leep, [q]uit, [u]p, [d]own, [f]orward, [b]ackward, [st]ep size (meters), [r]otate (deg), [p]oint: "
         cmd = input(cmd_str).lower()
         if cmd == 'q' or cmd == 'quit':
             return False
@@ -72,14 +73,27 @@ class Controller:
         elif cmd == 'u' or cmd == 'up':
             self.move(DIRECTION.UP, self.step_size)
         elif cmd == 'd' or cmd == 'down':
-            self.move(DIRECTION.UP, self.step_size)
+            self.move(DIRECTION.DOWN, self.step_size)
         elif cmd == 'f' or cmd == 'forward':
-            self.move(DIRECTION.UP, self.step_size)
+            self.move(DIRECTION.FORWARD, self.step_size)
         elif cmd == 'b' or cmd == 'backward':
-            self.move(DIRECTION.UP, self.step_size)
+            self.move(DIRECTION.BACKWARD, self.step_size)
+        elif cmd == 'r' or cmd == 'rotate':
+            angle_deg = input(
+                f"Please enter the amount of degrees you want to rotate (absolute position): ")
+            angle = float(angle_deg)
+            self.move(DIRECTION.ROTATE, angle)
         elif cmd == 'st' or cmd == 'step_size':
-            new_step_size = input(f"Please enter a new step size [cm]: ")
-            self.step_size = new_step_size
+            new_step_size = input(f"Please enter a new step size [m]: ")
+            self.step_size = float(new_step_size)
+        elif cmd == 'p' or cmd == 'point':
+            point = input(
+                f"Please input an absolute point [cm] as a tuple of (x,y,z): ")
+            points = point.replace("(", "").replace(")", "").split(",")
+            points = [float(p) / 100 for p in points]
+            self.move_to_point(points)
+        else:
+            print(f"Invalid cmd given: {cmd}")
         self.update_joint_angle_dict()
         return True
 
@@ -93,29 +107,30 @@ class Controller:
         return self.robot.arm.get_joint_commands()
 
     def move(self, dir: DIRECTION, relative_distance: float):
+        current_ee_pos = self.ee_pos()
         match dir:
             case DIRECTION.UP:
-                pass
+                self.robot.arm.set_ee_pose_components(
+                    current_ee_pos[0], current_ee_pos[1], current_ee_pos[2] + relative_distance)
             case DIRECTION.DOWN:
-                pass
+                self.robot.arm.set_ee_pose_components(
+                    current_ee_pos[0], current_ee_pos[1], current_ee_pos[2] - relative_distance)
             case DIRECTION.FORWARD:
-                pass
+                self.robot.arm.set_ee_pose_components(
+                    current_ee_pos[0] + relative_distance, current_ee_pos[1], current_ee_pos[2])
             case DIRECTION.BACKWARD:
-                pass
-            case DIRECTION.LEFT:  # Rotate left
-                pass
-            case DIRECTION.RIGHT:  # Rotate right
-                pass
+                self.robot.arm.set_ee_pose_components(
+                    current_ee_pos[0] - relative_distance,  current_ee_pos[1], current_ee_pos[2])
+            case DIRECTION.ROTATE:
+                return self.robot.arm.set_single_joint_position(
+                    "waist", np.deg2rad(relative_distance))
             case _:
-                pass
+                print(f"Invalid movement direction given")
+                return False
 
-    def move_toward(self, point):
-        # Get current ee position
-        # Get X, Y, Z displacement betweeen current and desired point and set trajectory
-        current_pos = self.ee_pos()
-        self.robot.arm.set_ee_cartesian_trajectory(point[0] - current_pos[0],
-                                                   point[1] - current_pos[1],
-                                                   point[2] - current_pos[2],)
+    def move_to_point(self, point):
+        self.robot.arm.set_ee_pose_components(
+            x=point[0], y=point[1], z=point[2])
 
     def forward_kinematics(self, joint_positions):
         T = mr.FKinSpace(self.robot.arm.robot_des.M,
